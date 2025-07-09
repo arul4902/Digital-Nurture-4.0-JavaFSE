7aa8948bf92ac55bd2edc7ec159c52a938dd9448
ARUL S <kumarsiva42887@gmail.com>
2025-07-09 22:01:32 +0530

    Completed week 4 hands-on

---
+++ Week4_Spring_REST_using_Spring_Boot 3/01_spring_rest_handson/02 Spring Core – Load SimpleDateFormat from Spring Configuration XML/Code/spring-learn-h2/src/main/java/com/cognizant/spring_learn_h2/SpringLearnH2Application.java
@@ -0,0 +1,19 @@
+package com.cognizant.spring_learn_h2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringLearnH2Application {
+    public static void main(String[] args) throws Exception {
+        displayDate();
+    }
+
+    public static void displayDate() throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+        Date date = format.parse("31/12/2018");
+        System.out.println("Parsed Date: " + date);
+    }
+}
---
+++ Week4_Spring_REST_using_Spring_Boot 3/01_spring_rest_handson/02 Spring Core – Load SimpleDateFormat from Spring Configuration XML/Code/spring-learn-h2/src/test/java/com/cognizant/spring_learn_h2/SpringLearnH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
