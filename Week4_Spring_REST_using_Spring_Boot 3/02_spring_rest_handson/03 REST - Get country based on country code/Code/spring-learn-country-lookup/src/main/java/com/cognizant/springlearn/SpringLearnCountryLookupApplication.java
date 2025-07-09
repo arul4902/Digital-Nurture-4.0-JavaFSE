ecd9235a01363a0b4013bfbc588e286015c92f7d
ARUL S <kumarsiva42887@gmail.com>
2025-07-10 00:39:34 +0530

    Completed week 4 hands-on

---
+++ Week4_Spring_REST_using_Spring_Boot 3/02_spring_rest_handson/03 REST - Get country based on country code/Code/spring-learn-country-lookup/src/main/java/com/cognizant/springlearn/SpringLearnCountryLookupApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnCountryLookupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnCountryLookupApplication.class, args);
+    }
+}
